69e55067167d62126bc96972f6d0379be55d6c6c
Maximilian Dösch <doesch.maximilian@gmail.com>
2023-09-06 14:51:59 +0200

    first test node

--- src/TrackerNode.cpp
+++ src/TrackerNode.cpp
@@ -1,6 +1,35 @@
 #include "TrackerNode.h"
 
-TrackerNode::TrackerNode() : _tracker(_max_hypotheses)
+#include <pcl_conversions/pcl_conversions.h>
+
+TrackerNode::TrackerNode() : _tracker(_max_hypotheses), _visualization(0.001, 40), _lastIteration{ros::Time::now()}
 {
-    
+    _pointSub = this->subscribe(_pointTopic, 1, &TrackerNode::_rcvPointCallback, this);
+}
+
+void TrackerNode::_rcvPointCallback(const sensor_msgs::PointCloud2::ConstPtr & pointMsg)
+{
+    double time_period_ms = (ros::Time::now() - _lastIteration).toNSec() / 1000000.;
+    _lastIteration = ros::Time::now();
+
+    _tracker.predict(time_period_ms);
+
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(*pointMsg, *pointcloud);
+
+    if(pointcloud->size() > 0)
+        _tracker.update(pointcloud);
+
+    _tracker.reduce();
+
+    std::vector<validation::ValidationModel*> models;
+    _tracker.estimate(models);
+
+    _visualization.draw(pointcloud, models);
+
+    for(validation::ValidationModel* v_model : models)
+    {
+        delete v_model;
+    }
 }
--- src/TrackerNode.h
+++ src/TrackerNode.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 
 #include "tracker/Tracker.h"
+#include "validation/Visualization.h"
 
 class TrackerNode : ros::NodeHandle
 {
@@ -11,10 +13,17 @@ class TrackerNode : ros::NodeHandle
         void run() {ros::spin();}
 
     private:
+        void _rcvPointCallback(const sensor_msgs::PointCloud2::ConstPtr & pointMsg);
+
         tracker::PMBM _tracker;
+        validation::Visualization _visualization;
 
         ros::Subscriber _pointSub;
-        
+        ros::Publisher _boxPub;
+
+        ros::Time _lastIteration;
 
         int const _max_hypotheses = 10;
+
+        std::string const _pointTopic = "/rt40/floor_segmented";
 };
